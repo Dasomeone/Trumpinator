b6a23170b0876835734b8aaa3afdb77ff0863ec8
Emily Ahlstrand Rager <emilyahrager@gmail.com>
2017-03-18 21:24:00 +0000

    Added rudimentary database searching.

--- backend/concat.py
+++ backend/concat.py
@@ -1,7 +1,12 @@
 ## Library Imports
 import os
 import argparse
+import string
+import sqlite3
 
+## Setup SQLite connection and cursor
+conn = sqlite3.connect('../trump.db')
+c = conn.cursor()
 
 ## Setup Argparse for niceness
 parser = argparse.ArgumentParser(description='Process Text-To-Speech commands')
@@ -10,6 +15,20 @@ parser.add_argument('textstring')
 ## Parse the arguments
 args = parser.parse_args()
 
-# DEBUG # Printing the text string to show we received input correctly.
-print args.textstring
+# Remove all punctuation from input, and make it lowercase
+input = args.textstring.translate(None, string.punctuation).lower()
 
+# Split input sentence by spaces and store as Iterable.
+input_list = input.split(" ")
+
+# DEBUG # Printing the text string to show we received input correctly.
+print input_list
+
+
+## Iterate the sentence and query the database in one go, to find files.
+for w in input_list:
+	c.execute("SELECT location FROM words WHERE word=?", (w,))  # (w,) is a hack
+	result = c.fetchone()  # Fetch the result
+	print "word: \"{}\" located in file {}".format(w, result[0])
+
+conn.commit()  # Gracefully end connection with server.
--- backend/dbtest.py
+++ backend/dbtest.py
@@ -1,12 +1,24 @@
 import sqlite3
 
 ## Select DB
-conn = sqlite3.connect('../trump.db')
+conn = sqlite3.connect('../test.db')
 
 ## Create cursor 
 c = conn.cursor()
 
-c.execute("INSERT INTO words (word, location) VALUES ('test','testlocation')")
+c.execute("INSERT INTO words (word, location) VALUES ('i', '1.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('am', '2.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('the','3.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('best','4.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('china','5.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('great','6.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('america','7.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('united','8.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('states','9.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('of','10.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('test','11.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('kingdom','12.mp4')")
+c.execute("INSERT INTO words (word, location) VALUES ('working','13.mp4')")
 
 conn.commit()
 
