0f238dfa0d459b3841de463dbb2b4a46da7df7d1
Emily Ahlstrand Rager <emilyahrager@gmail.com>
2017-03-18 22:11:49 +0000

    Updated Database search to return list of location files

--- backend/concat.py
+++ backend/concat.py
@@ -23,12 +23,25 @@ input_list = input.split(" ")
 
 # DEBUG # Printing the text string to show we received input correctly.
 print input_list
-
+location_list = list()
 
 ## Iterate the sentence and query the database in one go, to find files.
 for w in input_list:
 	c.execute("SELECT location FROM words WHERE word=?", (w,))  # (w,) is a hack
 	result = c.fetchone()  # Fetch the result
-	print "word: \"{}\" located in file {}".format(w, result[0])
+	if(result == None):  # Word not in database.
+		location_list.append(None)
+	else:
+		location_list.append("{}".format(result[0]))
+
+	# DEBUG #
+	#print "word: \"{}\" located in file {}".format(w, result[0])
+
+# Debug # Print location list
+print location_list	
+
+# Check if words are missing. If they are flag it.
+if None in location_list:
+	print "Nonetype in locationlist" ## TODO: ADD FLAG.
 
 conn.commit()  # Gracefully end connection with server.
