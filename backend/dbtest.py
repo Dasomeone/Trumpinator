6521f913ea8a3f9793f9a6658e757d31f2615c20
Emily Ahlstrand Rager <emilyahrager@gmail.com>
2017-03-18 23:57:35 +0000

    Updated content

--- backend/dbtest.py
+++ backend/dbtest.py
@@ -6,8 +6,19 @@ conn = sqlite3.connect('../trump.db')
 ## Create cursor 
 c = conn.cursor()
 
-c.execute("INSERT INTO words (word, location) VALUES ('china', '../data/China.ts')")
-
+#c.execute("INSERT INTO words (word, location) VALUES ('china', '../data/China.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('back', '../data/intermediary/back.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('car', '../data/intermediary/car.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('china', '../data/intermediary/China.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('great', '../data/intermediary/great.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('in', '../data/intermediary/in.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('itis', '../data/intermediary/itis.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('leadership', '../data/intermediary/leadership.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('made', '../data/intermediary/made.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('suppliers', '../data/intermediary/Suppliers.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('surrender', '../data/intermediary/Surrender.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('truly', '../data/intermediary/truly.mp4.ts')")
+c.execute("INSERT INTO words (word, location) VALUES ('wonderful', '../data/intermediary/wonderful.mp4.ts')")
 
 conn.commit()
 
