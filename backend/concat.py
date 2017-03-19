14498b2eef8c443485d1ed071e0d19f5bdba6321
Emily Ahlstrand Rager <emilyahrager@gmail.com>
2017-03-19 05:41:37 +0000

    Updated Database with MAGA speech

--- backend/concat.py
+++ backend/concat.py
@@ -4,6 +4,7 @@ import argparse
 import string
 import sqlite3
 import subprocess
+import random
 
 ## Setup SQLite connection and cursor
 conn = sqlite3.connect('../trump.db')
@@ -23,23 +24,22 @@ input = args.textstring.translate(None, string.punctuation).lower()
 input_list = input.split(" ")
 
 # DEBUG # Printing the text string to show we received input correctly.
-print input_list
+#print input_list
 location_list = list()
 
 ## Iterate the sentence and query the database in one go, to find files.
 for w in input_list:
 	c.execute("SELECT location FROM words WHERE word=?", (w,))  # (w,) is a hack
-	result = c.fetchone()  # Fetch the result
-	if(result == None):  # Word not in database.
-		location_list.append(None)
-	else:
-		location_list.append("{}".format(result[0]))
+	result = c.fetchall()  # Fetch the result (Single result)
+	if(result != None): 
+		chosen_loc = random.choice(result)[0] # If there is more than one of the same word, pick one at random.
+		location_list.append("{}".format(chosen_loc))
 
 	# DEBUG #
 	#print "word: \"{}\" located in file {}".format(w, result[0])
 
 # Debug # Print location list
-print location_list	
+#print location_list	
 
 # Sanitize list from "Nonetype" values. 
 location_list = list(filter(lambda a: a != None, location_list))
