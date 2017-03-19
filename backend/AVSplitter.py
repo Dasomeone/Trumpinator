a9b2a5ff591c762991f2eaef82b8654238cc4b43
Emily Ahlstrand Rager <emilyahrager@gmail.com>
2017-03-19 02:45:22 +0000

    Fixed AV Splitter to accurately subdivide clips.

--- backend/AVSplitter.py
+++ backend/AVSplitter.py
@@ -23,12 +23,16 @@ for r in data['results']:
 	if cur_phrase['confidence'] > 0.7:
 		for word in cur_phrase['timestamps']:
 			#if word[0] == "build":
-			#	print "Word: {}, Start_time: {}, End_time: {}, Duration: {}".format(word[0].lower(), word[1], word[2], word[2]-word[1])
-			start_time = word[1]+0.5
-			duration = word[2]-start_time
-			file_out = '../data/intermediary/{}-{}.ts'.format(word[0],start_time)
-			subprocess.call(['ffmpeg', '-y', '-ss', '{}'.format(start_time), '-i', '{}'.format(MEDIA_IN_FILE), '-t', '{}'.format(duration), '-c', 'copy', '-bsf:v', 'h264_mp4toannexb', '-f', 'mpegts', '{}'.format(file_out)])
-			c.execute("INSERT INTO words (word, location) VALUES (?, ?)", (word[0], file_out,))	
+			#	print "Word: {}, Start_time: {}, End_time: {}, Duration: {}".format(word[0].lower(), start_time, word[2], duration)
+			duration = word[2]-word[1]
+			start_time = word[1]
+
+			file_out = '../data/intermediary/{}-{}'.format(word[0],start_time)
+			SQL_FILE_OUT = file_out + ".ts"
+			subprocess.call(['ffmpeg', '-y', '-ss', '{}'.format(start_time), '-i', '{}'.format(MEDIA_IN_FILE), '-t', '{}'.format(duration), '{}.mp4'.format(file_out)]) ## Export subcut
+			subprocess.call(['ffmpeg', '-y', '-i', '{}.mp4'.format(file_out), '-c', 'copy', '-bsf:v', 'h264_mp4toannexb', '-f', 'mpegts', '{}.ts'.format(file_out)])
+			subprocess.call(['rm', '{}.mp4'.format(file_out)])
+			c.execute("INSERT INTO words (word, location) VALUES (?, ?)", (word[0].lower(), SQL_FILE_OUT,))	
 
 		# Confidence is good in the transcript
 		# Continue gathering dataset to split file.
